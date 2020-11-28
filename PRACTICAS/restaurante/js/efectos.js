9fdd858b1065687c5c07f3320baaa142da80b014
Brayan Sánchez <bs6961204@gmail.com>
2020-11-28 17:23:47 -0600

    PRACTICA DE RESTAURANTE
    se ha terminado la practica del sitio web para un restaurante y se pasara a la sección de bootstrap

---
+++ PRACTICAS/restaurante/js/efectos.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+
+/*===================================
+=            EFECTO MENU            =
+===================================*/
+
+	$('.menu a').each(function(index, elemento) {
+        $(this).css({
+            'top': '-200px'
+        });
+
+        $(this).animate({
+            top: '0'
+        }, 2000 + (index * 500));
+    })
+
+/*=====  End of EFECTO MENU  ======*/
+
+/*=====================================
+=            EFECTO HEADER            =
+=====================================*/
+
+    if ($(window).width() > 800){
+        $('header .textos').css({
+            opacity: 0,
+            marginTop: 0
+        });
+
+        $('header .textos').animate({
+            opacity: 1,
+            marginTop: '-52px'
+        },1500);
+    }
+
+/*=====  End Of EFECTO HEADER  ======*/
+
+/*=============================================
+=            SCROLL ELEMENTOS MENU            =
+=============================================*/
+
+var acercaDe = $('#acerca-de').offset().top,
+    menu = $('#platillos').offset().top,
+    galeria = $('#galeria').offset().top,
+    ubicacion = $('#ubicacion').offset().top;
+
+$('#btn-acerca-de').on('click', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: 380
+    }, 500)
+});
+
+$('#btn-menu').on('click', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: menu
+    }, 500)
+});
+
+$('#btn-galeria').on('click', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: galeria
+    }, 500)
+});
+
+$('#btn-ubicacion').on('click', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: ubicacion
+    }, 500)
+});
+
+/*=====  End of SCROLL ELEMENTOS MENU  ======*/
+
+});
